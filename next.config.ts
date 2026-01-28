c861e9e43d297c9f27d00cffaae948234ad23f7b
dragonfore4 <kemkaikub@gmail.com>
2026-01-28 22:18:59 +0700

    feat (host) : allow cloudflare image url host

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "pub-89a7d3afd9574669a02c5f4fce069c51.r2.dev",
+        port: "",
+        pathname: "/**", // อนุญาตทุกโฟลเดอร์ใน R2 นี้
+      },
+    ],
+  },
 };
 
 export default nextConfig;
